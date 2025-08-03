8eb3b292a9809c60ee1bdc6c93b3ed7009969ba4
Talha Ghauri <talhaghauridev@gmail.com>
2025-08-03 15:57:54 +0500

    add env.ts

---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod/v4";
+
+export const env = createEnv({
+  server: {
+    GEMINI_API_KEY: z.string().nonempty(),
+    ARCJET_KEY: z.string().nonempty(),
+    CLERK_SECRET_KEY: z.string().nonempty(),
+  },
+
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().nonempty(),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().nonempty(),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().nonempty(),
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().nonempty(),
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().nonempty(),
+  },
+
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+  },
+});
