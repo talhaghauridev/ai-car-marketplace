5d48bc21f68686916962ed140db821e8dd1b0c9c
Talha Ghauri <talhaghauridev@gmail.com>
2025-08-03 15:58:26 +0500

    install packages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
   typescript: {
     ignoreBuildErrors: true,
   },
