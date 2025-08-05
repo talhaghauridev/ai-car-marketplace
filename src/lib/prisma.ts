6a9205a09baa74782dd1db3bb03229dbd596840c
Talha Ghauri <talhaghauridev@gmail.com>
2025-08-05 16:09:07 +0500

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,27 @@
+import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
+});
 
 export default prisma;
